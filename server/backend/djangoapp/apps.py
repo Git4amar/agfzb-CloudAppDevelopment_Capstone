b37818887a08b9c8443a0d90cbf7e20f74321eb2
Git4amar <103874177+Git4amar@users.noreply.github.com>
2023-01-27 21:07:29 -0800

    Delete server directory

--- server/backend/djangoapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoappConfig(AppConfig):
-    name = 'djangoapp'
-    default_auto_field = 'django.db.models.BigAutoField'
--- server/backend/djangoapp/static/djangoapp/dealer_table_filter.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-$(document).ready(function(){
-    $('#dealer_table').bootstrapTable()
-});
